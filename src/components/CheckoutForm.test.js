23d32b8d28a1add7d822b02e055e26e1d45cc5cd
Michael Guerrero <32268444+MikeLikesCode@users.noreply.github.com>
2021-08-06 13:03:44 -0400

    Initial commit

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,52 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm/>)
+    const header = screen.queryByText('Checkout Form')
+    expect(header).toBeInTheDocument();
+    expect(header).toBeTruthy();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", async () => {
+    render(<CheckoutForm/>)
+
+    const firstName = screen.queryByLabelText('First Name:')
+    userEvent.type(firstName, 'Mike');
+    expect(firstName).toHaveValue('Mike');
+
+    const lastName = screen.queryByLabelText('Last Name:');
+    userEvent.type(lastName, 'Guerrero');
+    expect(lastName).toHaveValue('Guerrero');
+
+    const address = screen.queryByLabelText('Address:');
+    userEvent.type(address, '9020 169th Street');
+    expect(address).toHaveValue('9020 169th Street');
+
+    const city = screen.queryByLabelText('City:');
+    userEvent.type(city, 'Jamaica');
+    expect(city).toHaveValue('Jamaica');
+
+    const state = screen.queryByLabelText('State:');
+    userEvent.type(state, 'NY');
+    expect(state).toHaveValue('NY');
+
+    const zip = screen.queryByLabelText('Zip:');
+    userEvent.type(zip, '11432');
+    expect(zip).toHaveValue('11432');
+
+    const submit = screen.getByRole("button");
+    userEvent.click(submit);
+    
+    const successMessage = screen.queryByTestId('successMessage').textContent
+
+    
+    expect(successMessage).toBeInTheDocument;
+    
+
+
+});
